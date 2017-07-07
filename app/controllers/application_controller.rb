15c9dfa90930858a60644b23fc87c7ee55063c7a
Melanie Williams <melaniepwilliams00@gmail.com>
2017-07-07 19:20:01 +0000

    add images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require './config/environment'
-require './app/models/sample_model'
+require './app/models/results'
 
 class ApplicationController < Sinatra::Base
   configure do
@@ -10,4 +10,18 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/results' do
+    foodz = params[:food].to_i
+    boroz = params[:boro].to_i
+    homez = params[:home].to_i
+    roomz = params[:roommates].to_i
+    subwayz = params[:subway].to_i
+    puts params
+    sum = find_sum(foodz, boroz, homez, roomz, subwayz)
+    puts sum
+    @spirit_animal = id_animal(sum)
+    erb :results
+  end
+  
 end
